3abc48fed5b7cea672db9b376d0ae7b95a842242
PAVEL KOZIN <sart6zaV@yandex.ru>
2025-07-10 09:48:01 +0300

    feat:input main

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,7 +1,44 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <></>;
+interface InputProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+class Input extends React.Component<InputProps> {
+  render() {
+    return (
+      <input
+        type="text"
+        value={this.props.value}
+        onChange={this.props.onChange}
+      />
+    );
+  }
+}
+
+class App extends React.Component<object, { inputValue: string }> {
+  constructor(props: object) {
+    super(props);
+    this.state = { inputValue: "" };
+  }
+
+  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="app-container">
+        <Input
+          value={this.state.inputValue}
+          onChange={this.handleInputChange}
+        />
+        <p>{this.state.inputValue}</p>
+      </div>
+    );
+  }
 }
 
 export default App;

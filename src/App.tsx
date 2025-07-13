400e431dda86bcc7e090d1c8521535f8b5fe6d93
PAVEL KOZIN <sart6zaV@yandex.ru>
2025-07-13 22:31:13 +0300

    fix: change api

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,50 @@ interface InputProps {
 }
 
 interface ButtonProps {
-  onClick?: () => void;
+  onClick: () => void;
   children: React.ReactNode;
+}
+
+interface PlanetProperties {
+  name: string;
+  diameter: string;
+  rotation_period: string;
+  orbital_period: string;
+  population: string;
+  climate: string;
+  terrain: string;
+}
+interface PlanetUrl {
+  url: string;
+  name: string;
+}
+
+interface Planet {
+  uid: string;
+  name: string;
+  properties: PlanetProperties;
+}
+
+interface PlanetsListItem {
+  uid: string;
+  name: string;
+  url: string;
+}
+
+interface PlanetsListResponse {
+  results: PlanetsListItem[];
+}
+
+interface AppState {
+  inputValue: string;
+  searchResults: Planet[];
 }
 
 class Button extends React.Component<ButtonProps> {
   render() {
-    return (
-      <button 
-        onClick={this.props.onClick}
-      >
-        {this.props.children}
-      </button>
-    );
+    return <button onClick={this.props.onClick}>{this.props.children}</button>;
   }
 }
-
-
 
 class Input extends React.Component<InputProps> {
   render() {
@@ -37,31 +64,104 @@ class Input extends React.Component<InputProps> {
   }
 }
 
-class App extends React.Component<object, { inputValue: string }> {
+interface PlanetDetailsResponse {
+  result: {
+    properties: PlanetProperties;
+    uid: string;
+  };
+}
+
+class App extends React.Component<object, AppState> {
+  apiUrl: string = "https://swapi.tech/api/planets";
+
   constructor(props: object) {
     super(props);
-    this.state = { inputValue: "" };
+    this.state = {
+      inputValue: "",
+      searchResults: [],
+    };
   }
-  
-  handleClick = () => {
+
+  fetchPlanets = async (searchQuery: string = "") => {
+    let url: string;
+    if (searchQuery) {
+      url = this.apiUrl + "?search=" + encodeURIComponent(searchQuery);
+    } else {
+      url = this.apiUrl;
+    }
+
+    const listResponse = await fetch(url);
+
+    const listData: PlanetsListResponse = await listResponse.json();
+    const planets = await Promise.all(
+      listData.results.map(async (planet: PlanetUrl) => {
+        const detailsResponse = await fetch(planet.url);
+        if (!detailsResponse.ok) {
+          throw new Error(`Failed`);
+        }
+        const detailsData: PlanetDetailsResponse = await detailsResponse.json();
+        return {
+          uid: detailsData.result.uid,
+          name: detailsData.result.properties.name,
+          properties: {
+            name: detailsData.result.properties.name,
+            diameter: detailsData.result.properties.diameter,
+            rotation_period: detailsData.result.properties.rotation_period,
+            orbital_period: detailsData.result.properties.orbital_period,
+            population: detailsData.result.properties.population,
+            climate: detailsData.result.properties.climate,
+            terrain: detailsData.result.properties.terrain,
+          },
+        };
+      }),
+    );
+
+    this.setState({ searchResults: planets });
   };
 
+  handleSearch = () => {
+    this.fetchPlanets(this.state.inputValue);
+  };
+
+  componentDidMount() {
+    this.fetchPlanets();
+  }
 
   handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     this.setState({ inputValue: e.target.value });
   };
 
   render() {
+    const { inputValue, searchResults } = this.state;
+
     return (
       <div className="app-container">
-        <Input
-          value={this.state.inputValue}
-          onChange={this.handleInputChange}
-        />
-        <p>{this.state.inputValue}</p>
-        <Button onClick={this.handleClick}>
-          Поиск
-        </Button>
+        <h1>Star Wars Planets</h1>
+
+        <div className="search-container">
+          <Input value={inputValue} onChange={this.handleInputChange} />
+          <Button onClick={this.handleSearch}>{"Search"}</Button>
+        </div>
+
+        <div className="results-container">
+          {searchResults.length != 0 ? (
+            <div className="no-results">Nothing</div>
+          ) : (
+            <ul className="results-list">
+              {searchResults.map((planet: Planet) => (
+                <li key={planet.uid}>
+                  <h3>{planet.name}</h3>
+                  <p>Diameter: {planet.properties.diameter}</p>
+                  <p>Climate: {planet.properties.climate}</p>
+                  <p>Terrain: {planet.properties.terrain}</p>
+                  <p>Population: {planet.properties.population}</p>
+                  <p>Rotation Period: {planet.properties.rotation_period}</p>
+                  <p>Orbital Period: {planet.properties.orbital_period}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
     );
   }

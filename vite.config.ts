7e4225b2a80671ac7ce96fb60ccfd3154e351197
PAVEL KOZIN <sart6zaV@yandex.ru>
2025-07-18 01:49:52 +0300

    init: config test

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,27 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   resolve: { preserveSymlinks: true },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
+    coverage: {
+      provider: "v8",
+      ignoreEmptyLines: true,
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+    include: ["src/**/*.{js,jsx,ts,tsx}"],
+    exclude: [
+      "src/**/*.test.{js,jsx,ts,tsx}",
+      "src/**/*.spec.{js,jsx,ts,tsx}",
+      "src/index.{js,jsx,ts,tsx}",
+      "src/setupTests.{js,ts}",
+      "src/**/*.d.ts",
+    ],
+  },
 });

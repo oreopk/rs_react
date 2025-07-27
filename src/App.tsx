8554f95b97cf8da53e90b17b66d1d13c8618ae7a
PAVEL KOZIN <sart6zaV@yandex.ru>
2025-07-27 03:43:19 +0300

    fix: change class to func in App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import ErrorButton from "./ErrorButton";
 import Button from "./Button";
@@ -6,6 +6,7 @@ import Input from "./Input";
 import PlanetCard from "./PlanetCard";
 import Spinner from "./Spinner";
 import { PlanetApi } from "./PlanetFetch";
+
 interface PlanetProperties {
   name?: string;
   diameter: string;
@@ -22,104 +23,84 @@ interface Planet {
   properties: PlanetProperties;
 }
 
-interface AppState {
-  inputValue: string;
-  searchResults: Planet[];
-  isLoading: boolean;
-  error: string | null;
-  throwBoolean: boolean;
-}
+function App(): React.ReactElement {
+  const localStorageKey: string = "starWarsQuery";
 
-class App extends React.Component<object, AppState> {
-  localStorageKey: string = "starWarsQuery";
+  const [inputValue, setInputValue] = useState<string>(
+    localStorage.getItem(localStorageKey) || "",
+  );
+  const [searchResults, setSearchResults] = useState<Planet[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [errorBoolean, setErrorBoolean] = useState<boolean>(false);
 
-  constructor(props: object) {
-    super(props);
-    this.state = {
-      inputValue: localStorage.getItem(this.localStorageKey) || "",
-      searchResults: [],
-      isLoading: false,
-      error: null,
-      throwBoolean: false,
-    };
+  function triggerError() {
+    setErrorBoolean(true);
   }
 
-  resetErrorState = () => {
-    this.setState({ throwBoolean: false });
-  };
-
-  triggerError = () => {
-    this.setState({ throwBoolean: true });
-  };
-
-  fetchPlanets = async (searchQuery: string = "") => {
+  const fetchPlanets = async (searchQuery: string = "") => {
     let errorMessage = "Unknown error";
-    this.setState({ isLoading: true, error: null });
+    setIsLoading(true);
+    setError(null);
     try {
       const planets = await PlanetApi.fetchPlanets(searchQuery);
-      this.setState({ searchResults: planets, isLoading: false });
+      setSearchResults(planets);
     } catch (error) {
       if (error instanceof Error) {
         errorMessage = error.message;
       } else if (typeof error === "string") {
         errorMessage = error;
       }
-      this.setState({ error: errorMessage, isLoading: false });
+      setError(errorMessage);
+    } finally {
+      setIsLoading(false);
     }
   };
 
-  handleSearch = () => {
-    localStorage.setItem(this.localStorageKey, this.state.inputValue.trim());
-    this.fetchPlanets(this.state.inputValue);
-  };
-
-  componentDidMount() {
-    this.setState({
-      inputValue: localStorage.getItem(this.localStorageKey) || "",
-    });
-    this.fetchPlanets(this.state.inputValue);
+  function handleSearch() {
+    localStorage.setItem(localStorageKey, inputValue.trim());
+    fetchPlanets(inputValue);
   }
 
-  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ inputValue: e.target.value });
-  };
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(e.target.value);
+  }
 
-  render() {
-    const { inputValue, searchResults, isLoading, throwBoolean } = this.state;
+  useEffect(() => {
+    setInputValue(localStorage.getItem(localStorageKey) || "");
+    fetchPlanets(inputValue);
+  }, []);
 
-    if (throwBoolean) {
-      throw new Error("Test error");
-    }
+  if (errorBoolean) {
+    throw new Error("Test error");
+  }
 
-    return (
-      <div className="app-container" data-testid="app">
-        <h1 className="title">Star Wars Planets</h1>
-        {this.state.error ? (
-          <div data-testid="error-message">{this.state.error}</div>
-        ) : null}
-        <div className="search-container">
-          <Input value={inputValue} onChange={this.handleInputChange} />
-          <Button onClick={this.handleSearch}>{"Search"}</Button>
-        </div>
-        {isLoading ? (
-          <Spinner />
-        ) : (
-          <div className="results-container">
-            {inputValue.trim() !== "" && searchResults.length === 0 ? (
-              <div className="nothing">Nothing</div>
-            ) : (
-              <div className="results-grid">
-                {searchResults.map((planet: Planet) => (
-                  <PlanetCard key={planet.uid} planet={planet} />
-                ))}
-              </div>
-            )}
-          </div>
-        )}
-        <ErrorButton onClick={this.triggerError} />
+  return (
+    <div className="app-container" data-testid="app">
+      <h1 className="title">Star Wars Planets</h1>
+      {error ? <div data-testid="error-message">{error}</div> : null}
+      <div className="search-container">
+        <Input value={inputValue} onChange={handleInputChange} />
+        <Button onClick={handleSearch}>{"Search"}</Button>
       </div>
-    );
-  }
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="results-container">
+          {inputValue.trim() !== "" && searchResults.length === 0 ? (
+            <div className="nothing">Nothing</div>
+          ) : (
+            <div className="results-grid">
+              {searchResults.map((planet: Planet) => (
+                <PlanetCard key={planet.uid} planet={planet} />
+              ))}
+            </div>
+          )}
+        </div>
+      )}
+      <ErrorButton onClick={triggerError} />
+    </div>
+  );
 }
 
 export default App;

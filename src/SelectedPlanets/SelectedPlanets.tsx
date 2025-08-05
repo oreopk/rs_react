b424fd14ad9040edb8d9afd02d4b0488bf8b7d4c
PAVEL KOZIN <sart6zaV@yandex.ru>
2025-08-06 01:36:12 +0300

    fix: show selected plantes to  selected quantity

--- src/SelectedPlanets/SelectedPlanets.tsx
+++ src/SelectedPlanets/SelectedPlanets.tsx
@@ -1,4 +1,3 @@
-import type { PlanetsListItem } from "../types.ts";
 import { useAppSelector } from "../hooks/hooks.ts";
 import { stateItems } from "../store/selectedItemsSlice.ts";
 import "./SelectedPlanets.css";
@@ -9,14 +8,7 @@ export default function SelectedPlanets() {
 
   return (
     <div className={`selected-planets ${hasItems ? "visible" : ""}`}>
-      <h3>Selected Planets</h3>
-      <div className="selected-planets-list">
-        {selectedItems.map((planet: PlanetsListItem) => (
-          <div key={planet.uid} className="selected-planet">
-            {planet.name}
-          </div>
-        ))}
-      </div>
+      <h3>Selected Planets: {selectedItems.length}</h3>
     </div>
   );
 }

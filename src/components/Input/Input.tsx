e24ada6883edac4225e5fe6a34e4648cb5e85938
PAVEL KOZIN <sart6zaV@yandex.ru>
2025-08-18 23:47:06 +0300

    fix: delete tests

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useContext } from "react";
-import { ThemeContext } from "../../App/[locale]/providers/ThemeContext";
+import { ThemeContext } from "../../../src/app/[locale]/providers/ThemeContext";
 
 interface InputProps {
   value: string;

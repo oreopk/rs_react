e494171ca20dd7a7601d3c570e775856764ace49
PAVEL KOZIN <sart6zaV@yandex.ru>
2025-08-31 04:07:34 +0300

    feat: add extra key button list

---
+++ src/components/Modal.tsx
@@ -0,0 +1,19 @@
+import { createPortal } from 'react-dom';
+
+type Props = {
+  open: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+};
+
+export default function Modal({ open, onClose, children }: Props) {
+  if (!open) return null;
+  return createPortal(
+    <div className="wrapper_modal" onClick={onClose}>
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>,
+    document.body
+  );
+}
--- src/components/Page_co2.tsx
+++ src/components/Page_co2.tsx
@@ -1,27 +1,30 @@
+import Modal from './Modal';
 import { useEffect, useState, type JSX } from 'react';
+import type { extraRow } from './types';
+import { extra_key } from './types';
+
+type Row = {
+  [key: string]: number | string;
+};
 
 type Location = {
   iso_code: string;
-  data: { year: number; population: number }[];
+  data: extraRow[];
 };
 type datajson = Record<string, Location>;
-type YearRow = { year: number; population?: number };
 
-function lastPopulation(data: YearRow[]) {
+function lastFieldValue(data: Row[], key: string, year: number) {
   for (let i = data.length - 1; i >= 0; i--) {
-    const population = data[i].population;
-    if (population) {
-      return {
-        year: data[i].year,
-        population: Number(population),
-      };
-    }
+    const row = data[i] as Record<string, number | string | null | undefined>;
+    if (row.year != year) continue;
+    return row[key];
   }
-  return { year: [], population: [] };
 }
 
 export default function Page_co2() {
   const [data, setData] = useState<datajson | null>(null);
+  const [open, setOpen] = useState(false);
+  const [extraCols, setExtraCols] = useState<string[]>([]);
 
   useEffect(() => {
     fetch('/owid-co2-data.json')
@@ -34,6 +37,21 @@ export default function Page_co2() {
       .then(setData);
   }, []);
 
+  function toggleCol(key: string) {
+    setExtraCols((prev) => {
+      if (prev.includes(key)) {
+        return prev.filter((x) => {
+          if (x === key) {
+            return false;
+          }
+          return true;
+        });
+      }
+
+      return [...prev, key];
+    });
+  }
+
   const rows: JSX.Element[] = [];
 
   for (const key in data) {
@@ -41,30 +59,78 @@ export default function Page_co2() {
     if (!node) continue;
     const name = key;
     const iso = node.iso_code;
-    const { year, population } = lastPopulation(node.data);
+    const population = lastFieldValue(node.data, 'population', 2023);
+    const year = lastFieldValue(node.data, 'year', 2023);
     rows.push(
       <tr key={key + iso}>
         <td>{name}</td>
         <td>{iso}</td>
         <td>{population}</td>
         <td>{year}</td>
+        {extraCols.map((column) => (
+          <td key={column}>{lastFieldValue(node.data, column, 2023) ?? ''}</td>
+        ))}
       </tr>
     );
   }
   return (
     <>
       <div className="main_co2">
-        <table className="co2_table">
-          <thead>
-            <tr>
-              <th>Country</th>
-              <th>ISO</th>
-              <th>Population</th>
-              <th>Year</th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </table>
+        <div className="header">
+          <button onClick={() => setOpen(true)}>Select Columns</button>
+        </div>
+        {open && (
+          <Modal open={open} onClose={() => setOpen(false)}>
+            <div className="modal_actions">
+              <button className="btn_modal" onClick={() => setExtraCols([])}>
+                Clear all
+              </button>
+              <button
+                className="btn_modal"
+                onClick={() => setExtraCols(extra_key)}
+              >
+                Select all
+              </button>
+              <button className="btn_modal" onClick={() => setOpen(false)}>
+                X
+              </button>
+            </div>
+            <div className="modal_body">
+              <div className="checkbox_flex">
+                {extra_key.map((key) => (
+                  <label
+                    key={key}
+                    className={`check ${extraCols.includes(key) ? 'is-active' : ''}`}
+                  >
+                    <input
+                      type="checkbox"
+                      className="hidden_checkbox"
+                      checked={extraCols.includes(key)}
+                      onChange={() => toggleCol(key)}
+                    />
+                    <span className="check_label">{key}</span>
+                  </label>
+                ))}
+              </div>
+            </div>
+          </Modal>
+        )}
+        <div className="table_wrapper">
+          <table className="co2_table">
+            <thead>
+              <tr>
+                <th>Country</th>
+                <th>ISO</th>
+                <th>Population</th>
+                <th>Year</th>
+                {extraCols.map((column) => (
+                  <th key={column}>{column}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+          </table>
+        </div>
       </div>
     </>
   );
